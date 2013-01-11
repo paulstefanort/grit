7913343d7709bcea16b16f81caf4388cc1e4fded
Paul Stefan Ort <mail@paulstefanort.com>
2013-01-10 20:47:11 -0600

    rails new . -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grit::Application.config.secret_token = '88b7b91139474259f19ddda2c6052abe75626a261ea32b5b30e488f9498aaf1fbd0fd1069a7f95d8df42992f09a7fdcd9d4a47876dd4089915f112245ea11822'
