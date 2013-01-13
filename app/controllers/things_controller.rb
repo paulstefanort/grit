b891d6ed6ac0a5c9f0175b691daa389a317c5e40
Paul Stefan Ort <mail@paulstefanort.com>
2013-01-12 20:22:26 -0600

    Perform CRUD operations via Backbone.js, interacting with the Things controller.

---
+++ app/controllers/things_controller.rb
@@ -0,0 +1,25 @@
+class ThingsController < ApplicationController
+  def index
+		@things = Thing.all
+  end
+
+  def create
+		@thing = Thing.create(params[:thing])
+		render :json => @thing
+  end
+
+  def update
+		@thing = Thing.find(params[:id])
+		if params[:thing].has_key?(:content)
+			@thing.content = params[:thing][:content]
+		end
+		@thing.save
+		render :json => @thing
+  end
+
+  def destroy
+		@thing = Thing.find(params[:id])
+		@thing.delete
+		render :json => @thing
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Grit::Application.routes.draw do
   get "site/index"
 	root :to => "site#index"
+
+	resources :things do
+	end
 end
